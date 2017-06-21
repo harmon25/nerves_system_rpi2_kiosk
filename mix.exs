95b2d3557dfd14d09383527de20cdb10a20fc16a
Doug <harmon25@users.noreply.github.com>
2017-06-20 21:30:01 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesSystemRpi2.Mixfile do
   def project do
     [app: :nerves_system_rpi2,
      version: @version,
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      compilers: Mix.compilers ++ [:nerves_package],
      description: description(),
      package: package(),
@@ -33,9 +33,9 @@ defmodule NervesSystemRpi2.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+    [maintainers: ["harmon25"],
     files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md", "VERSION", "rootfs-additions", "fwup.conf", "cmdline.txt", "linux-4.4.defconfig", "config.txt", "post-createfs.sh"],
      licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/nerves-project/nerves_system_rpi2"}]
+     links: %{"Github" => "https://github.com/harmon25/nerves_system_rpi2_kiosk"}]
   end
 end
