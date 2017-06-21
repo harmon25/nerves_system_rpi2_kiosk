9b4aeb6b9d8898683e08e58333041db30ed6273f
harmon <nomraharmon@gmail.com>
2017-06-20 20:27:26 -0400

    need to launch udevd to get mouse and keyboard input

---
+++ rootfs-additions/env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export QT_QPA_EVDEV_MOUSE_PARAMETERS="/dev/input/event0;grab=1"
+export QT_QPA_EVDEV_KEYOARD_PARAMETERS="/dev/input/event1;grab1"
+export QT_QPA_EGLFS_HIDECURSOR=0
+export QT_QPA_EGLFS_WIDTH=1920
+export QT_QPA_EGLFS_HEIGHT=1080
